15693969d0a8115dc4f73ea7ad0db4c53c14e8e9
Yemilay Romero <yemilay.romero@gmail.com>
2016-05-03 13:55:26 -0500

    Refactor middleware

--- server.js
+++ server.js
@@ -2,17 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('privarte route hit!!');
-        next();
-    },
-    logger: function (req, res, next) {
-
-        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' +  req.originalUrl);
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
@@ -21,7 +11,7 @@ app.get('/about', middleware.requireAuthentication, function(req, res) {
 });
 
 app.use(express.static(__dirname + '/public'));
-// console.log(__dirname);
+
 app.listen(PORT, function () {
     console.log('Express server started on port ' + PORT + '!');
 });
